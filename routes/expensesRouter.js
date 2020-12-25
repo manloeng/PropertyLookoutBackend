7fbd5cd362f501e8aeb1ef9ad3ca18dd39f9e4b1
Andrew <37455284+manloeng@users.noreply.github.com>
2020-12-25 12:52:01 +0000

    Update Routes - Expense needs property id

--- routes/expensesRouter.js
+++
@@ -1,6 +0,0 @@
-const expenseRouter = require("express").Router();
-const addProjectExpenses = require("../controller/expenses/addProjectExpenses");
-
-const { methodNotAllowed } = require("../errors");
-
-expenseRouter.route("/").post(addProjectExpenses).all(methodNotAllowed);
--- routes/propertyRouter.js
+++ routes/propertyRouter.js
@@ -2,10 +2,12 @@ const propertyRouter = require("express").Router();
 const addProperty = require("../controller/property/addProperty");
 const getAllPropertiesByUserId = require("../controller/property/getAllPropertiesByUserId");
 const getPropertyByPropertyId = require("../controller/property/getPropertyByPropertyId");
+const addProjectExpenses = require("../controller/expenses/addProjectExpenses");
 const { methodNotAllowed } = require("../errors");
 
 // should get all properties without showing users id
 propertyRouter.route("/").get(getAllPropertiesByUserId).post(addProperty).all(methodNotAllowed);
 propertyRouter.route("/:property_id").get(getPropertyByPropertyId).all(methodNotAllowed);
+propertyRouter.route("/:property_id/expense").post(addProjectExpenses).all(methodNotAllowed);
 
 module.exports = propertyRouter;
