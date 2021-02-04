bb709984174f461f79117f61b30527e7db955dbd
Andrew <37455284+manloeng@users.noreply.github.com>
2021-02-04 21:19:19 +0000

    Add UpdatePropertyByPropertyId method

---
+++ controller/property/updatePropertyByPropertyId.js
@@ -0,0 +1,22 @@
+const Property = require("../../models/property/model");
+
+async function updatePropertyByPropertyId(req, res) {
+  try {
+    const { propertyId } = req.params;
+    const data = req.body;
+
+    const property = await Property.findByIdAndUpdate(
+      { _id: propertyId },
+      { $set: data },
+      { strict: false, upsert: true, multi: true }
+    ).exec();
+
+    if (property) {
+      return res.status(200).json(property);
+    } else return res.send({ msg: "Something Gone Wrong" });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = updatePropertyByPropertyId;
