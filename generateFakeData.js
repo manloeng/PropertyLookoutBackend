f7a1351649da31349996e09801c56c5169eb0f54
Andrew <37455284+manloeng@users.noreply.github.com>
2021-02-20 13:39:53 +0000

    Update fake data generator

--- generateFakeData.js
+++ generateFakeData.js
@@ -1,16 +1,6 @@
 const faker = require("faker");
 
 function getTestData(req, res) {
-  const rent = {
-    cost: faker.random.number({
-      min: 400,
-      max: 1200,
-    }),
-    startDate: new Date(faker.date.between("2021-01-01", "2021-03-30")),
-    endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
-  };
-  const copyRent = { ...rent };
-
   let dataArr = [];
   for (let id = 1; id <= 10; id++) {
     const purchasePrice = faker.random.number({
@@ -20,7 +10,7 @@ function getTestData(req, res) {
     const deposit = purchasePrice / 4;
 
     const data = {
-      uuid: "5fb3effadbe5216f6beee6c5",
+      account: "5fb3effadbe5216f6beee6c5",
       postCode: "M123AD",
       propertyNumber: faker.random.number({
         min: 1,
@@ -42,29 +32,50 @@ function getTestData(req, res) {
         min: 1,
         max: 3,
       }),
-      rental: {
-        currentRental: {
-          ...copyRent,
-          isRented: true,
-        },
-        EPC: {
-          rating: faker.random.arrayElement(["A", "B", "C", "D", "E"]),
-          startDate: new Date(faker.date.between("2021-01-01", "2021-02-05")),
-          endDate: new Date(faker.date.between("2022-01-05", "2023-11-05")),
-        },
-        gasCertificate: {
-          isRequired: false,
-        },
-        electricCertificate: {
-          startDate: new Date(faker.date.between("2021-01-01", "2021-01-05")),
-          endDate: new Date(faker.date.between("2022-01-05", "2025-11-05")),
-        },
-      },
     };
 
     dataArr.push(data);
   }
+  // const dataArr = getRental()
   res.send(dataArr);
 }
 
 module.exports = getTestData;
+
+function getRental() {
+  const rent = {
+    cost: faker.random.number({
+      min: 400,
+      max: 1200,
+    }),
+    startDate: new Date(faker.date.between("2021-01-01", "2021-03-30")),
+    endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
+  };
+  const copyRent = { ...rent };
+
+  let dataArr = [];
+  for (let id = 1; id <= 10; id++) {
+    const rental = {
+      account: "5fb3effadbe5216f6beee6c5",
+      property: "60310e34694c3400098dc19f",
+      currentRental: {
+        ...copyRent,
+        isRented: true,
+      },
+      EPC: {
+        rating: faker.random.arrayElement(["A", "B", "C", "D", "E"]),
+        startDate: new Date(faker.date.between("2021-01-01", "2021-02-05")),
+        endDate: new Date(faker.date.between("2022-01-05", "2023-11-05")),
+      },
+      gasCertificate: {
+        isRequired: false,
+      },
+      electricCertificate: {
+        startDate: new Date(faker.date.between("2021-01-01", "2021-01-05")),
+        endDate: new Date(faker.date.between("2022-01-05", "2025-11-05")),
+      },
+    };
+    dataArr.push(rental);
+  }
+  res.send(dataArr);
+}
