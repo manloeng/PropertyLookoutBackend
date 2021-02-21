08a81d6f653fd44e43c15b99ad9f2bddd1071884
Andrew <37455284+manloeng@users.noreply.github.com>
2021-02-21 14:06:15 +0000

    Update Routes for new db structure

--- routes/financeRouter.js
+++ routes/financeRouter.js
@@ -3,5 +3,48 @@ const getProjectFinancesByUserId = require("../controller/finances/getProjectFin
 const { methodNotAllowed } = require("../errors");
 
 financeRouter.route("/").get(getProjectFinancesByUserId).all(methodNotAllowed);
+financeRouter.route("/:propertyId").get(getProjectFinancesByPropertyId).all(methodNotAllowed);
+
+financeRouter
+  .route("/monthlyInc")
+  .get(getMonthlyIncome)
+  .patch(updateMonthlyIncome)
+  .delete(deleteMonthlyIncome)
+  .all(methodNotAllowed);
+
+financeRouter
+  .route("/oneOffInc")
+  .get(getOneOffIncome)
+  .patch(updateOneOffIncome)
+  .delete(deleteOneOffIncome)
+  .all(methodNotAllowed);
+
+financeRouter
+  .route("/monthlyCapitalExp")
+  .get(getMonthlyCapitalExpense)
+  .patch(updateMonthlyCapitalExpense)
+  .delete(deleteMonthlyCapitalExpense)
+  .all(methodNotAllowed);
+
+financeRouter
+  .route("/OneOffCapitalExp")
+  .get(getOneOffCapitalExpense)
+  .patch(updateOneOffCapitalExpense)
+  .delete(deleteOneOffCapitalExpense)
+  .all(methodNotAllowed);
+
+financeRouter
+  .route("/monthlyRevenueExp")
+  .get(getMonthlyRevenueExpense)
+  .patch(updateMonthlyRevenueExpense)
+  .delete(deleteMonthlyRevenueExpense)
+  .all(methodNotAllowed);
+
+financeRouter
+  .route("/OneOffRevenueExp")
+  .get(getOneOffRevenueExpense)
+  .patch(updateOneOffRevenueExpense)
+  .delete(deleteOneOffRevenueExpense)
+  .all(methodNotAllowed);
 
 module.exports = financeRouter;
