2ccc9aa07d1be36fad5147cdeaaffddfc390cc55
Andrew <37455284+manloeng@users.noreply.github.com>
2021-02-21 15:53:36 +0000

    Add updateFinance Method

---
+++ controller/finances/updateFinance.js
@@ -0,0 +1,18 @@
+const getModel = require("./utils/getModel");
+
+async function updateFinance(req, res) {
+  const Model = getModel(req.route.path);
+
+  try {
+    const { id } = req.params;
+    const data = req.body;
+
+    const updatedFinance = await Model.findOneAndUpdate({ id }, data).exec();
+
+    res.status(200).send({ updatedFinance });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = updateFinance;
