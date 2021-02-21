803033b63166972570460fa0c4027d4bd7eacfdf
Andrew <37455284+manloeng@users.noreply.github.com>
2021-02-21 15:00:49 +0000

    Update Schemas

--- models/oneOffCapitalExpense/schema.js
+++ models/oneOffCapitalExpense/schema.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 let OneOffCapitalExpense = new Schema({
-  property: { type: mongoose.Types.ObjectId(), required: true },
-  account: { type: mongoose.Types.ObjectId(), required: true },
+  property: { type: mongoose.Types.ObjectId, required: true },
+  account: { type: mongoose.Types.ObjectId, required: true },
   name: {
     type: String,
     required: true,
