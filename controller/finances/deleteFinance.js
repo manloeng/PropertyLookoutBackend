bb2781bfdb8f05e612b270c2bf4f38b1432b7670
Andrew <37455284+manloeng@users.noreply.github.com>
2021-02-21 15:54:41 +0000

    Add deleteFinance method

---
+++ controller/finances/deleteFinance.js
@@ -0,0 +1,17 @@
+const getModel = require("./utils/getModel");
+
+async function deleteFinance(req, res) {
+  const Model = getModel(req.route.path);
+
+  try {
+    const { id } = req.params;
+
+    await Model.findOneAndDelete({ id }).exec();
+
+    res.status(200).send({ msg: "Sucessfully deleted" });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = deleteFinance;
