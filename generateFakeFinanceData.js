7d2148ad0d1da285f13673cfe7ef8b082183a15f
Andrew <37455284+manloeng@users.noreply.github.com>
2021-02-22 21:17:11 +0000

    Update method to add data directly to finance

--- generateFakeFinanceData.js
+++ generateFakeFinanceData.js
@@ -1,19 +1,104 @@
 const faker = require("faker");
+const MonthlyCapitalExpense = require("./models/monthlyCapitalExpense/model");
+const MonthlyIncome = require("./models/monthlyIncome/model");
+const MonthlyRevenueExpense = require("./models/monthlyRevenueExpense/model");
+const OneOffCapitalExpense = require("./models/oneOffCapitalExpense/model");
+const OneOffIncome = require("./models/oneOffIncome/model");
+const OneOffRevenueExpense = require("./models/oneOffRevenueExpense/model");
 
 function generateFakeFinanceData(req, res) {
-  let dataArr = [];
-  for (let id = 1; id <= 100; id++) {
-    const data = {
-      account: "5fb3effadbe5216f6beee6c5",
-      property: "60310e34694c3400098dc19f",
-      name: faker.commerce.productName(),
-      cost: faker.commerce.price(),
-      startDate: new Date(faker.date.between("2020-01-01", "2021-03-30")),
-      endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
-    };
-    dataArr.push(data);
+  if (process.env.NODE_ENV === "development") {
+    for (let id = 1; id <= 10; id++) {
+      const data = {
+        account: "5fb3effadbe5216f6beee6c5",
+        property: "60310e34694c3400098dc19f",
+        name: faker.commerce.productName(),
+        cost: faker.commerce.price(),
+        startDate: new Date(faker.date.between("2020-01-01", "2021-03-30")),
+        endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
+      };
+      const monthlyCapitalExpense = new MonthlyCapitalExpense({ ...data });
+      monthlyCapitalExpense.save(function (err) {
+        if (err) console.log(err);
+      });
+    }
+
+    for (let id = 1; id <= 10; id++) {
+      const data = {
+        account: "5fb3effadbe5216f6beee6c5",
+        property: "60310e34694c3400098dc19f",
+        name: faker.commerce.productName(),
+        cost: faker.commerce.price(),
+        startDate: new Date(faker.date.between("2020-01-01", "2021-03-30")),
+        endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
+      };
+      const monthlyIncome = new MonthlyIncome({ ...data });
+      monthlyIncome.save(function (err) {
+        if (err) console.log(err);
+      });
+    }
+
+    for (let id = 1; id <= 10; id++) {
+      const data = {
+        account: "5fb3effadbe5216f6beee6c5",
+        property: "60310e34694c3400098dc19f",
+        name: faker.commerce.productName(),
+        cost: faker.commerce.price(),
+        startDate: new Date(faker.date.between("2020-01-01", "2021-03-30")),
+        endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
+      };
+      const monthlyRevenueExpense = new MonthlyRevenueExpense({ ...data });
+      monthlyRevenueExpense.save(function (err) {
+        if (err) console.log(err);
+      });
+    }
+
+    for (let id = 1; id <= 10; id++) {
+      const data = {
+        account: "5fb3effadbe5216f6beee6c5",
+        property: "60310e34694c3400098dc19f",
+        name: faker.commerce.productName(),
+        cost: faker.commerce.price(),
+        startDate: new Date(faker.date.between("2020-01-01", "2021-03-30")),
+        endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
+      };
+      const oneOffCapitalExpense = new OneOffCapitalExpense({ ...data });
+      oneOffCapitalExpense.save(function (err) {
+        if (err) console.log(err);
+      });
+    }
+
+    for (let id = 1; id <= 10; id++) {
+      const data = {
+        account: "5fb3effadbe5216f6beee6c5",
+        property: "60310e34694c3400098dc19f",
+        name: faker.commerce.productName(),
+        cost: faker.commerce.price(),
+        startDate: new Date(faker.date.between("2020-01-01", "2021-03-30")),
+        endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
+      };
+      const oneOffIncome = new OneOffIncome({ ...data });
+      oneOffIncome.save(function (err) {
+        if (err) console.log(err);
+      });
+    }
+
+    for (let id = 1; id <= 10; id++) {
+      const data = {
+        account: "5fb3effadbe5216f6beee6c5",
+        property: "60310e34694c3400098dc19f",
+        name: faker.commerce.productName(),
+        cost: faker.commerce.price(),
+        startDate: new Date(faker.date.between("2020-01-01", "2021-03-30")),
+        endDate: new Date(faker.date.between("2022-01-01", "2023-11-05")),
+      };
+      const oneOffRevenueExpense = new OneOffRevenueExpense({ ...data });
+      oneOffRevenueExpense.save(function (err) {
+        if (err) console.log(err);
+      });
+    }
   }
-  res.send(dataArr);
+  res.send({ msg: "successs" });
 }
 
 module.exports = generateFakeFinanceData;
