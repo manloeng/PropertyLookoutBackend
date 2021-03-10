e4a7f31147bae38748718db14674cc6ef0988192
Andrew <37455284+manloeng@users.noreply.github.com>
2021-03-10 23:03:44 +0000

    Add comment

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ mongoose.set("useFindAndModify", false);
 connectToMongoose();
 
 app.use(cors());
+console.log(cors(), "cors enabled");
 // var whitelist = ["http://localhost:6010", "https://property-lookout.vercel.app/"];
 // const corsOptions = {
 //   origin: function (origin, callback) {
