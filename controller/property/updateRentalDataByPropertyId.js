b962c249cc8343883663f2987e1bbf40f0c5f2e2
Andrew <37455284+manloeng@users.noreply.github.com>
2021-03-10 21:09:24 +0000

    Add updateRentalData Method

---
+++ controller/property/updateRentalDataByPropertyId.js
@@ -0,0 +1,19 @@
+const Rental = require("../../models/rental/model");
+
+async function updateRentalDataByPropertyId(req, res) {
+  try {
+    const { propertyId } = req.params;
+    const data = req.body;
+
+    const documentExist = await Rental.exists({ property: propertyId });
+
+    if (documentExist) {
+      const rental = await Rental.findOneAndUpdate({ property: propertyId }, data, { new: true }).exec();
+      return res.status(200).json(rental);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = updateRentalDataByPropertyId;
--- routes/propertyRouter.js
+++ routes/propertyRouter.js
@@ -8,6 +8,7 @@ const addProjectFinances = require("../controller/finances/addProjectFinances");
 const deleteProjectFinances = require("../controller/finances/deleteProjectFinances");
 const getProjectFinanceByPropertyId = require("../controller/finances/getProjectFinanceByPropertyId");
 const getRentalDataByPropertyId = require("../controller/property/getRentalDataByPropertyId");
+const updateRentalDataByPropertyId = require("../controller/property/updateRentalDataByPropertyId");
 const { methodNotAllowed } = require("../errors");
 
 // should get all properties without showing users id
@@ -28,6 +29,10 @@ propertyRouter
   .all(methodNotAllowed);
 
 // could do queries instead?
-propertyRouter.route("/:propertyId/rental").get(getRentalDataByPropertyId).all(methodNotAllowed);
+propertyRouter
+  .route("/:propertyId/rental")
+  .get(getRentalDataByPropertyId)
+  .patch(updateRentalDataByPropertyId)
+  .all(methodNotAllowed);
 
 module.exports = propertyRouter;
