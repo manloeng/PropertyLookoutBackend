03d76177c42e24490f1a2c11c10cdbfcd3e1a611
Andrew <37455284+manloeng@users.noreply.github.com>
2021-05-29 22:45:51 +0100

    Add deleteUser Method

---
+++ controller/user/deleteUser.js
@@ -0,0 +1,18 @@
+const User = require("../../models/user/model.js");
+
+async function deleteUser(req, res) {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.find({ _id: userId }).exec();
+
+    if (user) {
+      await User.deleteOne({ _id: userId }).exec();
+      res.status(201).send({ msg: "Sucessfully deleted property" });
+    }
+  } catch (e) {
+    res.send("error...");
+  }
+}
+
+module.exports = deleteUser;
