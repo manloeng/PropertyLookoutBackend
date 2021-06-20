07a54b42659840733fa780b114e0f81a07297b9c
Andrew <37455284+manloeng@users.noreply.github.com>
2021-06-20 13:51:50 +0100

    Update: Account Schema

--- models/accounts/schema.js
+++ models/accounts/schema.js
@@ -3,13 +3,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 let Accounts = new Schema(
   {
-    username: {
-      type: String,
+    userId: {
+      type: mongoose.Types.ObjectId,
       required: true,
     },
     password: {
       type: String,
       required: true,
+    },
+
+    providerType: {
+      type: String,
+    },
+
+    providerId: {
+      type: String,
+    },
+
+    providerAccountId: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+    accessToken: {
+      type: String,
     },
   },
   { timestamps: true }
