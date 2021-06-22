326cefe6367bc0dba72aabb0967f97a7bd5b99db
Andrew <37455284+manloeng@users.noreply.github.com>
2021-06-22 13:18:13 +0100

    Update: Users model

--- models/users/schema.js
+++ models/users/schema.js
@@ -1,38 +1,48 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-let Users = new Schema({
-  account: { type: mongoose.Types.ObjectId, required: true },
-  name: {
-    type: String,
+let Users = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    propertyNumber: {
+      type: Number,
+      required: true,
+    },
+    streetName: {
+      type: String,
+    },
+    area: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    postCode: {
+      type: String,
+      required: true,
+    },
+    propertyList: {
+      type: Array,
+    },
+    propertiesManaged: {
+      type: Array,
+    },
   },
-  phoneNumber: {
-    type: Number,
-    required: true,
-  },
-  propertyNumber: {
-    type: Number,
-    required: true,
-  },
-  streetName: {
-    type: String,
-  },
-  area: {
-    type: String,
-  },
-  city: {
-    type: String,
-  },
-  postCode: {
-    type: String,
-    required: true,
-  },
-  propertyList: {
-    type: Array,
-  },
-  propertiesManaged: {
-    type: Array,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = Users;
