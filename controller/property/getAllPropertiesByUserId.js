aaa56a88269fca1a8e68a2ff8e236b4770850e9f
Andrew <37455284+manloeng@users.noreply.github.com>
2021-06-28 10:13:34 +0100

    Update: Show property rental data

--- controller/property/getAllPropertiesByUserId.js
+++ controller/property/getAllPropertiesByUserId.js
@@ -1,10 +1,20 @@
 const Properties = require("../../models/property/model.js");
+const Rental = require("../../models/rental/model.js");
 
 async function getAllPropertiesByUserId(req, res) {
   try {
     const { userId } = req;
 
-    const properties = await Properties.find({ userId });
+    const properties = await Properties.find({ userId }).lean();
+
+    for (let i = 0; i < properties.length; i++) {
+      const property = properties[i];
+      const id = property._id;
+      const rental = await Rental.findOne({ property: id });
+
+      console.log(rental, "rental");
+      rental.currentRental ? (property.currentRental = rental.currentRental) : (property.currentRental = {});
+    }
 
     return res.status(200).json(properties);
   } catch (err) {
