d9205ce3d07473f118fe8037e779938297f73a94
Andrew <manloengchung@yahoo.co.uk>
2021-12-03 14:21:50 +0000

    Update: getFinance method

--- controller/finances/getFinance.js
+++ controller/finances/getFinance.js
@@ -2,18 +2,8 @@ const Finance = require("../../models/finance/model.js");
 
 async function getFinance(req, res) {
   try {
-    const { account } = req;
-    const { startDate } = req.query;
-
-    if (startDate) {
-      const newStartDate = new Date(startDate);
-      const currentYear = newStartDate.getFullYear();
-      query = { $gte: `${currentYear}-01-01` };
-    }
-
-    const response = await Finance.find({ account })
-      .sort({ startDate: 1 })
-      .lean();
+    const query = setupQuery(req);
+    const response = await Finance.find(query).sort({ startDate: 1 }).lean();
 
     return res.status(200).json(response);
   } catch (err) {
@@ -22,3 +12,24 @@ async function getFinance(req, res) {
 }
 
 module.exports = getFinance;
+
+function setupQuery(req) {
+  const { account } = req;
+  const { type = "", propertyId } = req.params;
+  const { startDate, endDate = "" } = req.query;
+
+  let queries = [{ account }];
+  if (type) queries.append({ type });
+  if (propertyId) queries.append({ property: propertyId });
+
+  const newStartDate = new Date(startDate);
+  const year = newStartDate.getFullYear();
+  if (startDate) queries.append({ startDate: { $gte: `${year}-01-01` } });
+
+  const newEndDate = new Date(endDate);
+  const endYear = newEndDate.getFullYear();
+  if (endDate) queries.append({ endDate: { $lte: `${endYear}-01-01` } });
+
+  if (queries.length) return { $and: queries };
+  else return {};
+}
