c9ee56ef9b78fce97fbc8307c59126a6f6460ee2
Andrew <manloengchung@yahoo.co.uk>
2021-12-03 14:28:58 +0000

    Update: Fix issues

--- controller/finances/getFinance.js
+++ controller/finances/getFinance.js
@@ -15,20 +15,23 @@ module.exports = getFinance;
 
 function setupQuery(req) {
   const { account } = req;
-  const { type = "", propertyId } = req.params;
+  const path = req.path;
+  const { propertyId } = req.params;
   const { startDate, endDate = "" } = req.query;
 
   let queries = [{ account }];
-  if (type) queries.append({ type });
-  if (propertyId) queries.append({ property: propertyId });
+
+  let type = path.slice(1);
+  if (type) queries.push({ type });
+  if (propertyId) queries.push({ property: propertyId });
 
   const newStartDate = new Date(startDate);
   const year = newStartDate.getFullYear();
-  if (startDate) queries.append({ startDate: { $gte: `${year}-01-01` } });
+  if (startDate) queries.push({ startDate: { $gte: `${year}-01-01` } });
 
   const newEndDate = new Date(endDate);
   const endYear = newEndDate.getFullYear();
-  if (endDate) queries.append({ endDate: { $lte: `${endYear}-01-01` } });
+  if (endDate) queries.push({ endDate: { $lte: `${endYear}-01-01` } });
 
   if (queries.length) return { $and: queries };
   else return {};
