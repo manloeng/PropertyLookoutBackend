f63e07eeac731cd0835567fbb35754abb5fa247f
Andrew <manloengchung@yahoo.co.uk>
2021-12-03 11:06:32 +0000

    Update: Method to use property id

--- controller/finances/getProjectFinanceByPropertyId.js
+++ controller/finances/getProjectFinanceByPropertyId.js
@@ -1,50 +1,27 @@
-const monthlyCapitalExpense = require("../../models/monthlyCapitalExpense/model.js");
-const monthlyIncome = require("../../models/monthlyIncome/model.js");
-const monthlyRevenueExpense = require("../../models/monthlyRevenueExpense/model.js");
-const oneOffCapitalExpense = require("../../models/oneOffCapitalExpense/model.js");
-const oneOffIncome = require("../../models/oneOffIncome/model.js");
-const oneOffRevenueExpense = require("../../models/oneOffRevenueExpense/model.js");
+const Finance = require("../../models/finance/model.js");
 
 async function getProjectFinanceByPropertyId(req, res) {
   try {
+    const { account } = req;
     const { propertyId } = req.params;
     const { startDate } = req.query;
-
-    let finances = {};
     let query = { property: propertyId };
-
-    const financeArray = [
-      monthlyCapitalExpense,
-      monthlyIncome,
-      monthlyRevenueExpense,
-      oneOffCapitalExpense,
-      oneOffIncome,
-      oneOffRevenueExpense,
-    ];
-
-    const financeStringArray = [
-      "monthlyCapitalExpense",
-      "monthlyIncome",
-      "monthlyRevenueExpense",
-      "oneOffCapitalExpense",
-      "oneOffIncome",
-      "oneOffRevenueExpense",
-    ];
 
     if (startDate) {
       const newStartDate = new Date(startDate);
       const currentYear = newStartDate.getFullYear();
-      query = { $and: [{ property: propertyId }, { startDate: { $gte: `${currentYear}-01-01` } }] };
+      query = {
+        $and: [
+          { account },
+          { property: propertyId },
+          { startDate: { $gte: `${currentYear}-01-01` } },
+        ],
+      };
     }
 
-    for (let i = 0; i < financeArray.length; i++) {
-      const finance = financeArray[i];
-      const response = await finance.find(query).sort({ startDate: 1 }).lean();
+    const response = await Finance.find(query).sort({ startDate: 1 }).lean();
 
-      finances[financeStringArray[i]] = response;
-    }
-
-    return res.status(200).json(finances);
+    return res.status(200).json(response);
   } catch (err) {
     console.log(err);
   }
