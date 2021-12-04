b0f65ef99dfb8935ef7fa3d20bd33f9802a5cf9f
Andrew <manloengchung@yahoo.co.uk>
2021-12-04 14:04:24 +0000

    Refactor: Rename file

--- controller/property/getProperty.js
+++ controller/property/getProperty.js
@@ -1,15 +1,13 @@
 const Property = require("../../models/property/model");
 
-async function getPropertyByPropertyId(req, res) {
+async function getProperty(req, res) {
   try {
     const { propertyId } = req.params;
-
     const property = await Property.find({ _id: propertyId }).exec();
-
     return res.status(200).json(property);
   } catch (e) {
     console.log(e);
   }
 }
 
-module.exports = getPropertyByPropertyId;
+module.exports = getProperty;
