0969293275d6b37b20c51e356e00f614d03ff0ca
Andrew <manloengchung@yahoo.co.uk>
2021-12-04 16:04:39 +0000

    Fix: Bug with routing

--- routes/financeRouter.js
+++ routes/financeRouter.js
@@ -7,16 +7,17 @@ const deleteFinance = require("../controller/finances/deleteFinance");
 const { methodNotAllowed } = require("../errors");
 
 financeRouter.route("/").get(getFinances).all(methodNotAllowed);
+
+financeRouter
+  .route("/calculations")
+  .get(analyseFinancialData)
+  .all(methodNotAllowed);
+
 financeRouter
   .route("/:financeId")
   .get(getFinance)
   .patch(updateFinance)
   .delete(deleteFinance)
-  .all(methodNotAllowed);
-
-financeRouter
-  .route("/calculations")
-  .get(analyseFinancialData)
   .all(methodNotAllowed);
 
 module.exports = financeRouter;
