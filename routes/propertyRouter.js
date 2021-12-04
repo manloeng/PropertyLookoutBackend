a59e019028477feb880161f93c8499a2714cc3d5
Andrew <manloengchung@yahoo.co.uk>
2021-12-04 15:42:13 +0000

    Add route to add finance

--- routes/propertyRouter.js
+++ routes/propertyRouter.js
@@ -4,6 +4,7 @@ const addProperty = require("../controller/property/addProperty");
 const getProperty = require("../controller/property/getProperty");
 const updateProperty = require("../controller/property/updateProperty");
 const deleteProperty = require("../controller/property/deleteProperty");
+const addFinance = require("../controller/finances/addFinance");
 const { methodNotAllowed } = require("../errors");
 
 propertyRouter
@@ -15,6 +16,7 @@ propertyRouter
 propertyRouter
   .route("/:propertyId")
   .get(getProperty)
+  .post(addFinance)
   .patch(updateProperty)
   .delete(deleteProperty)
   .all(methodNotAllowed);
