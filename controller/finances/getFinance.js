6ccee15cbe9ce37da0d46847cecc3d43f216497f
Andrew <manloengchung@yahoo.co.uk>
2021-12-05 13:04:00 +0000

    Fix: Get Finance Method

--- controller/finances/getFinance.js
+++ controller/finances/getFinance.js
@@ -2,9 +2,14 @@ const Finance = require("../../models/finance/model");
 
 async function getFinance(req, res) {
   try {
+    const { account } = req;
     const { financeId } = req.params;
-    const property = await Finance.find({ _id: financeId }).exec();
-    return res.status(200).json(property);
+
+    const finance = await Finance.find({
+      $and: [{ account }, { _id: financeId }],
+    }).exec();
+
+    return res.status(200).json(finance);
   } catch (e) {
     console.log(e);
   }
