5e786e77b60f525fcf2db7459c07d3b571b7c892
Andrew <manloengchung@yahoo.co.uk>
2021-12-05 13:06:16 +0000

    Update: to use account ID

--- controller/property/getProperty.js
+++ controller/property/getProperty.js
@@ -2,8 +2,13 @@ const Property = require("../../models/property/model");
 
 async function getProperty(req, res) {
   try {
+    const { account } = req;
     const { propertyId } = req.params;
-    const property = await Property.find({ _id: propertyId }).exec();
+
+    const property = await Property.find({
+      $and: [{ account }, { _id: propertyId }],
+    }).exec();
+
     return res.status(200).json(property);
   } catch (e) {
     console.log(e);
