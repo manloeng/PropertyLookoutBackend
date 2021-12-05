eee15316ad03cc245b3737989b0f1e896b785053
Andrew <manloengchung@yahoo.co.uk>
2021-12-05 13:10:12 +0000

    Refactor code: pull out method

--- controller/finances/getFinances.js
+++ controller/finances/getFinances.js
@@ -1,4 +1,5 @@
 const Finance = require("../../models/finance/model.js");
+const setupQuery = require("./utils/setupQuery");
 
 async function getFinances(req, res) {
   try {
@@ -12,25 +13,3 @@ async function getFinances(req, res) {
 }
 
 module.exports = getFinances;
-
-function setupQuery(req) {
-  const { account } = req;
-  const { propertyId } = req.params;
-  const { startDate, endDate = "", type = "" } = req.query;
-
-  let queries = [{ account }];
-
-  if (type) queries.push({ type });
-  if (propertyId) queries.push({ property: propertyId });
-
-  const newStartDate = new Date(startDate);
-  const year = newStartDate.getFullYear();
-  if (startDate) queries.push({ startDate: { $gte: `${year}-01-01` } });
-
-  const newEndDate = new Date(endDate);
-  const endYear = newEndDate.getFullYear();
-  if (endDate) queries.push({ endDate: { $lte: `${endYear}-01-01` } });
-
-  if (queries.length) return { $and: queries };
-  else return {};
-}
