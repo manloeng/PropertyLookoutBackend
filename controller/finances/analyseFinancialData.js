9fe2a2f04798cd9c3df297c2edb979128fa1d098
Andrew <manloengchung@yahoo.co.uk>
2021-12-05 16:01:49 +0000

    Update: Code to handle single and multiple req

--- controller/finances/analyseFinancialData.js
+++ controller/finances/analyseFinancialData.js
@@ -11,27 +11,29 @@ const {
   getLoanToValue,
   getTotalEquity,
 } = require("./utils/calculations");
+const setupQuery = require("./utils/setupQuery");
 
+// getting all, but i need only 1
 async function analyseFinancialData(req, res) {
-  const yearlyFinance = await Finance.find({}).lean();
-  const grossIncome = getGrossIncome(yearlyFinance);
-  const netIncome = getNetIncome(yearlyFinance);
-  const averageMonthlyIncome = getAverageMonthlyIncome(yearlyFinance);
-  const averageMonthlyExpense = getAverageMonthlyExpense(yearlyFinance);
+  const query = setupQuery(req);
+  const finance = await Finance.find(query).lean();
+  const grossIncome = getGrossIncome(finance);
+  const netIncome = getNetIncome(finance);
+  const averageMonthlyIncome = getAverageMonthlyIncome(finance);
+  const averageMonthlyExpense = getAverageMonthlyExpense(finance);
 
   // Get property data in order to full calculations
-  const properties = await Property.find({}).lean();
+  const properties = await Property.find(query).lean();
   const totalPurchasePrice = Object.values(properties).reduce(
     (previousKey, key) => previousKey + key.purchasePrice,
     0
   );
 
-  const allFinance = await Finance.find({}).lean();
-  const returnOnInvestment = getReturnOnInvestment(allFinance);
-  const grossYield = getGrossYield(allFinance, totalPurchasePrice);
-  const netYield = getNetYield(allFinance, totalPurchasePrice);
-  const loanToValue = getLoanToValue(allFinance, totalPurchasePrice);
-  const totalEquity = getTotalEquity(allFinance);
+  const returnOnInvestment = getReturnOnInvestment(finance);
+  const grossYield = getGrossYield(finance, totalPurchasePrice);
+  const netYield = getNetYield(finance, totalPurchasePrice);
+  const loanToValue = getLoanToValue(finance, totalPurchasePrice);
+  const totalEquity = getTotalEquity(finance);
 
   const dataAnalysis = {
     grossIncome,
