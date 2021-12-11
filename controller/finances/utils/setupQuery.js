7fc0124b3253828ed5dc82f7e9198aef7eb42a70
Andrew <manloengchung@yahoo.co.uk>
2021-12-11 15:19:12 +0000

    Fix: Query setup method - just uses date

--- controller/finances/utils/setupQuery.js
+++ controller/finances/utils/setupQuery.js
@@ -1,20 +1,13 @@
 function setupQuery(req) {
   const { account } = req;
   const { propertyId } = req.params;
-  const { startDate, endDate = "", type = "" } = req.query;
+  const { date = "", type = "" } = req.query;
 
   let queries = [{ account }];
 
   if (type) queries.push({ type });
   if (propertyId) queries.push({ property: propertyId });
-
-  const newStartDate = new Date(startDate);
-  const year = newStartDate.getFullYear();
-  if (startDate) queries.push({ startDate: { $gte: `${year}-01-01` } });
-
-  const newEndDate = new Date(endDate);
-  const endYear = newEndDate.getFullYear();
-  if (endDate) queries.push({ endDate: { $lte: `${endYear}-01-01` } });
+  if (date) queries.push({ date: { $gte: new Date(date) } });
 
   if (queries.length) return { $and: queries };
   else return {};
