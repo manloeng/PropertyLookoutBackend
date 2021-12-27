6d5e6be39ba42aa75f00d7f63017ce0b5d2a5a81
Andrew <manloengchung@yahoo.co.uk>
2021-12-27 14:50:04 +0000

    Update: Update finance method

--- controller/finances/updateFinance.js
+++ controller/finances/updateFinance.js
@@ -2,13 +2,17 @@ const Finance = require("../../models/finance/model");
 
 async function updateFinance(req, res) {
   try {
-    const { id } = req.params;
+    const { financeId } = req.params;
     const data = req.body;
 
-    const updatedFinance = await Finance.findOneAndUpdate({ _id: id }, data, {
-      strict: false,
-      new: true,
-    }).exec();
+    const updatedFinance = await Finance.findOneAndUpdate(
+      { _id: financeId },
+      data,
+      {
+        strict: false,
+        new: true,
+      }
+    ).exec();
 
     res.status(200).send({ updatedFinance });
   } catch (e) {
